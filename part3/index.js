31e23c12eb828831021165f3dfeaed47415e0ca1
Trevor <toantu3005@gmail.com>
2024-07-05 20:43:14 +1000

    updated part2

--- part3/index.js
+++ part3/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
@@ -19,7 +20,18 @@ let notes = [
     }
 ]
 
+app.use(cors())
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method);
+    console.log('Path: ', request.path);
+    console.log('Body: ', request.body);
+    console.log('---');
+    next()
+}
+
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -73,6 +85,14 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: 'unknown endpoint'
+    })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
