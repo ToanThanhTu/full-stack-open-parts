5d2c31232a040fd0cdba8559efdaa03398ecd6ea
Trevor <toantu3005@gmail.com>
2024-07-07 19:53:01 +1000

    part 3 added fly.io

--- part3/index.js
+++ part3/index.js
@@ -94,7 +94,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+// using the port defined in the environment variable or port 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
