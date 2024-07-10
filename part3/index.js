ed8d188bb34cd747c36ad145c16edf136e250776
Trevor <toantu3005@gmail.com>
2024-07-10 21:15:16 +1000

    build: rebuild dist
    feat: add mongoose validations for post and put

--- part3/index.js
+++ part3/index.js
@@ -20,23 +20,19 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (body.content === undefined) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/', (request, response) => {
@@ -62,14 +58,13 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        { content, important }, 
+        { new: true, runValidators: true, context: 'query' } // context: 'query' ensures validators run in the context of the query
+    )
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -97,6 +92,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
